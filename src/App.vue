c235d5782306e3e9e4bb9881a42cb43de4b57244
Rodrigo Dominguez <rodrigo.dominguez@alumnos.ucn.cl>
2021-03-02 17:40:24 -0300

    act 4

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-center">Historias</h1>
+    <Historia :historia="this.$data.alex"></Historia>
+    <hr>
+    <Historia :historia="this.$data.juan"></Historia>
+    <hr>
+    <div class="row">
+      <div class="col col-6 p-4">
+        <input type="text" class="form-control" v-model='filtro' placeholder="Buscar...">
+      </div>
+    </div>
+    <div class="alert alert-danger" role="alert" v-bind:key="item.historia" v-for="item in mostrarFiltro">
+      <b>{{item.nombre}} dice: </b>"{{item.historia}}"
+    </div>
+    <p ></p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Historia from './components/Historia';
+import EventBus from './bus'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Historia},
+  computed: {
+    mostrarFiltro(){
+      let historiasJuntas = this.juan;
+      let juntar = historiasJuntas.concat(this.alex) 
+      return juntar.filter(item => item.historia.includes(this.filtro));
+    },
+  },
+  
+  data(){return{
+    filtro: '',
+    juan: [ {nombre: 'Juan', historia: 'hola juan'},
+            {nombre: 'Juan', historia: 'Esta es una nueva historia de juan'},
+    ],
+    alex: [ {nombre: 'Alex', historia: 'Esta es una historia de alex'},
+            {nombre: 'Alex', historia: 'Nueva historia de alex.'},
+    ],
+    Juntar: [],
+
+  }},
+  created: function(){
+    EventBus.$on('add-historia', (data) =>{
+      if (data[1] == 'Alex') {
+        this.alex.push({nombre: data[1], historia: data[0]});
+      }else{
+        this.juan.push({nombre: data[1], historia: data[0]});
+      }
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Historia.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h4>Historias de {{historia[0].nombre}}</h4>
+        <div class="row">
+            <div class="col col-6 p-3"><input type="text" class="form-control" v-model="history" v-on:keyup.enter="addHistoria" placeholder="Agregar nueva historia..."></div>
+        </div>
+
+        <div  v-bind:key="data.historia" v-for="data of historia" class="alert alert-primary" role="alert">
+            <b>{{historia[0].nombre}} dice: </b> "{{data.historia}}"
+        </div>
+    </div>
+</template>
+
+<script>
+import EventBus from '../bus'
+export default {
+    name: 'Historia',
+    props:['historia'],
+    data(){
+        return{
+            history: ''
+        }
+    },
+    methods:{
+        addHistoria: function(){
+            EventBus.$emit('add-historia', [this.history, this.historia[0].nombre]);
+            this.history = '';
+        }
+    }
+}
+</script>
